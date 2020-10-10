55cd2e38467306aa3da7446c06d083eef3547bba
Rhoda Essien <rhorose@live.com>
2020-10-09 23:36:14 -0400

    Added squares

---
+++ tic-tac-toe.js
@@ -0,0 +1,10 @@
+window.onload= function(){
+ 
+    var gameboard= document.querySelectorAll('#board div');
+
+    for (let i=0; i<gameboard.length;i++){gameboard[i].classList.add("square");}
+
+
+ 
+
+}
